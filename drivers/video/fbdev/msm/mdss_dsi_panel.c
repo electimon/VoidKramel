1af1fad78d89f9820ba72ed75efeed602c305457
wangyq13 <wangyq13@lenovo.com>
2018-06-14 13:56:19 +0800

    msm: mdss: add panel_reg_read_lp_enable flag
    Remark:
    add panel_reg_read_lp_enable flag.
    Some IC such as FOCAL FT8006U, read reg lp mode only.
    Beause kernel 4.9 requirement, add return if ctrl is null,
    or pinfo will be used uninitialized on full path.
    (cherry-pick from bb2f0ca4fec013a4aada8053cca61cb68d9bfeb8)
    
    Change-Id: Ie7ae834b25dd1a0f6dafa4e9eacc3f419b2ea74f
    Signed-off-by: Yeqing Wang <wangyq13@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1193235
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Jun Weng <wengjun1@motorola.com>
    Reviewed-by: Shanshan Dai <daiss1@lenovo.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key
    (cherry picked from commit a4df1983fcc2e2c3c7becf3860b5251bb0c94a18)
    Reviewed-on: https://gerrit.mot.com/1196242
    Reviewed-by: Dezhen Hou <houdz1@motorola.com>
    SLTApproved: Dezhen Hou <houdz1@motorola.com>
    Submit-Approved: Dezhen Hou <houdz1@motorola.com>
    Tested-by: Dezhen Hou <houdz1@motorola.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -3093,6 +3093,8 @@ static int mdss_panel_parse_dt(struct device_node *np,
 					"qcom,mdss-dsi-lp11-init");
 	pinfo->panel_off_rst_disable = of_property_read_bool(np,
 					"qcom,mdss-panel-off-rst-disable");
+	pinfo->panel_reg_read_lp_enable = of_property_read_bool(np,
+					"qcom,mdss-panel-reg-read-lp-enable");
 	rc = of_property_read_u32(np, "qcom,mdss-dsi-init-delay-us", &tmp);
 	pinfo->mipi.init_delay = (!rc ? tmp : 0);
 
@@ -3187,11 +3189,14 @@ static int mdss_dsi_panel_reg_read(struct mdss_panel_data *pdata,
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata, panel_data);
 	pr_debug("%s: Reading %zu bytes from 0x%02x\n", __func__, size, reg);
 
+	if (!ctrl)
+		return -EINVAL;
+
 	memset(&cmdreq, 0, sizeof(cmdreq));
 	cmdreq.cmds = &reg_read_cmd;
 	cmdreq.cmds_cnt = 1;
 	cmdreq.flags = CMD_REQ_RX | CMD_REQ_COMMIT;
-	if (hs_mode)
+	if (!pinfo->panel_reg_read_lp_enable && hs_mode)
 		cmdreq.flags |= CMD_REQ_HS_MODE;
 	cmdreq.rlen = size;
 	cmdreq.cb = NULL; /* call back */
