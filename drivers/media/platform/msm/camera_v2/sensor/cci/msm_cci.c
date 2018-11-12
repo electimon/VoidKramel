7b9a3eaf87c3ffa4185c0b7c4131ace9a87a6a26
Yan Zhang <zhangy26@motorola.com>
2018-11-12 15:34:53 +0800

    channel : Add mutex protection to avoid cci contest
    This patch 871055 is released from QCOM, but qcom sugegsts to
    only apply this patch on product with CCI contest, such as
    low performance Channel.
    
    Change-Id: I04218653ca0d04046bbe58dedcc33adf72a69f1a
    Signed-off-by: Yan Zhang <zhangy26@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1246027
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key

--- drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
+++ drivers/media/platform/msm/camera_v2/sensor/cci/msm_cci.c
@@ -1676,9 +1676,16 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 	struct msm_camera_cci_ctrl *cci_ctrl)
 {
 	int32_t rc = 0;
+#ifdef CONFIG_MSM_CAMERA_CCI_MUTEX
+	struct cci_device *cci_dev;
+#endif
 
 	CDBG("%s line %d cmd %d\n", __func__, __LINE__,
 		cci_ctrl->cmd);
+#ifdef CONFIG_MSM_CAMERA_CCI_MUTEX
+	cci_dev = v4l2_get_subdevdata(sd);
+	mutex_lock(&cci_dev->mutex);
+#endif
 	switch (cci_ctrl->cmd) {
 	case MSM_CCI_INIT:
 		rc = msm_cci_init(sd, cci_ctrl);
@@ -1687,6 +1694,10 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 		rc = msm_cci_release(sd);
 		break;
 	case MSM_CCI_I2C_READ:
+#ifdef CONFIG_MSM_CAMERA_CCI_MUTEX
+		if (cci_dev->cci_state == CCI_STATE_DISABLED)
+			break;
+#endif
 		rc = msm_cci_i2c_read_bytes(sd, cci_ctrl);
 		break;
 	case MSM_CCI_I2C_WRITE:
@@ -1694,6 +1705,10 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 	case MSM_CCI_I2C_WRITE_SYNC:
 	case MSM_CCI_I2C_WRITE_ASYNC:
 	case MSM_CCI_I2C_WRITE_SYNC_BLOCK:
+#ifdef CONFIG_MSM_CAMERA_CCI_MUTEX
+		if (cci_dev->cci_state == CCI_STATE_DISABLED)
+			break;
+#endif
 		rc = msm_cci_write(sd, cci_ctrl);
 		break;
 	case MSM_CCI_GPIO_WRITE:
@@ -1707,6 +1722,9 @@ static int32_t msm_cci_config(struct v4l2_subdev *sd,
 	}
 	CDBG("%s line %d rc %d\n", __func__, __LINE__, rc);
 	cci_ctrl->status = rc;
+#ifdef CONFIG_MSM_CAMERA_CCI_MUTEX
+	mutex_unlock(&cci_dev->mutex);
+#endif
 	return rc;
 }
 
@@ -2103,6 +2121,9 @@ static int msm_cci_probe(struct platform_device *pdev)
 		pr_err("%s: no enough memory\n", __func__);
 		return -ENOMEM;
 	}
+#ifdef CONFIG_MSM_CAMERA_CCI_MUTEX
+	mutex_init(&new_cci_dev->mutex);
+#endif
 	v4l2_subdev_init(&new_cci_dev->msm_sd.sd, &msm_cci_subdev_ops);
 	snprintf(new_cci_dev->msm_sd.sd.name,
 			ARRAY_SIZE(new_cci_dev->msm_sd.sd.name), "msm_cci");
@@ -2200,6 +2221,9 @@ cci_invalid_vreg_data:
 cci_release_mem:
 	msm_camera_put_reg_base(pdev, new_cci_dev->base, "cci", true);
 cci_no_resource:
+#ifdef CONFIG_MSM_CAMERA_CCI_MUTEX
+	mutex_destroy(&new_cci_dev->mutex);
+#endif
 	kfree(new_cci_dev);
 	return rc;
 }
